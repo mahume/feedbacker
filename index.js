a9519d2b70d50febc7869f37f8f063aecd13bfd6
mahume <michael.a.hume@gmail.com>
2020-01-16 22:53:40 -0700

    Edit to prettier, adding test routes

--- index.js
+++ index.js
@@ -2,10 +2,34 @@ const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
+const { googleClientID, googleClientSecret } = require("./config/keys");
+
 const app = express();
 
 // New instance of Strategy
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: googleClientID,
+      clientSecret: googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+// Test route for OAuth
+// Initial route to hit
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+// Callback URL Route with code
+app.get("auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
