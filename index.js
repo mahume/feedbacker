0afec1a3905625b2508a0ceefc46165ee71b62f3
mahume <michael.a.hume@gmail.com>
2020-01-17 22:28:51 -0700

    adding cookie functionality for passport

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const authRoutes = require("./routes/authRoutes");
-const { mongoURI } = require("./config/keys");
+const { mongoURI, cookieKey } = require("./config/keys");
 require("./models/User");
 require("./services/passport");
 
@@ -13,6 +15,17 @@ mongoose.connect(mongoURI, {
 
 // Create Express instance
 const app = express();
+
+// Enable cookies within app
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [cookieKey],
+  })
+);
+// Enable Passport to use cookies for OAuth
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Use Auth routes
 authRoutes(app);
