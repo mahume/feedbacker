27c0a724e88314f8cb77e415b2026ce36e381f20
mahume <michael.a.hume@gmail.com>
2020-01-17 22:12:24 -0700

    edit / refactor

--- services/passport.js
+++ services/passport.js
@@ -14,9 +14,13 @@ passport.use(
       clientSecret: googleClientSecret,
       callbackURL: "/auth/google/callback",
     },
-    (accessToken, refreshToken, profile, done) => {
-      // Create Model instance then save it to DB
-      new User({ googleId: profile.id }).save();
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        new User({ googleId: profile.id }).save().then(user => done(null, user));
+      }
     }
   )
 );
