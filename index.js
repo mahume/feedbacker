cf9a9a671f74d36d554043f082153c03be531fe9
mahume <michael.a.hume@gmail.com>
2020-01-17 20:52:43 -0700

    Mongoose install and connect

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose"):
 const authRoutes = require("./routes/authRoutes");
+const { mongoURI } = require("./config/keys");
 require("./services/passport");
 
+mongoose.connect(mongoURI);
 const app = express();
 authRoutes(app);
 
